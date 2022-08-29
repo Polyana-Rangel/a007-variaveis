7fc76dc4aae6fe7475c65938d7705bf8aba4d1e2
Polyana-Rangel <polyanarangel17@gmail.com>
2022-08-29 17:09:53 -0300

    exercicio2

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+// let faculdade = confirm ("voce vai para a faculdade hoje?")
+// let exercicio = confirm ("voce fez todos os exercicios?")
+// let dificildade = confirm (" achou facil?")
+
+// console.log(faculdade)
+// console.log(exercicio)
+// console.log(dificildade)
+
+const perguntaFaculdade = "voce vai para a faculdade hoje?"
+const respostaFaculdade = prompt(perguntaFaculdade)
+const perguntaExecicio = " voce fez todos os exercicios?"
+const respostaExercicio = prompt(perguntaExecicio)
+const perguntaDificuldade = "achou facil?"
+const respostaDificuldade = prompt(perguntaDificuldade)
+
+console.log(perguntaFaculdade, respostaFaculdade)
+console.log(perguntaExecicio, respostaExercicio)
+console.log(perguntaDificuldade, respostaDificuldade)
